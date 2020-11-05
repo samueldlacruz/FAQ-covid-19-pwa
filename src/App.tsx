e6e1f82a19e33c9d15b6a7898d04c405ad644c56
samuel <samueldlacg@gmail.com>
2020-11-05 15:40:08 -0400

    initial commit and config styled component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { GlobalStyle } from './globalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <p>klk</p>
+    </>
+  );
+}
+
+export default App;
