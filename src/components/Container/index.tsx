21e3de4d1d8c52b518422bb20058b117926bef4c
Samuel De la cruz <s.delacruz@tele.finance>
2021-06-29 19:31:13 -0400

    feat: rename style files

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container } from './Container.styles';
+import { Container } from './styles';
 import Header from '../Header'
 import Footer from '../Footer'
 import FAQList from '../FAQList';
